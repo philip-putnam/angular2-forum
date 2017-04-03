5d2f26cdad5488a355ba2b00dbac61f299ccaa29
Erica Wright and Philip Putnam <philip@>
2017-04-03 10:28:12 -0700

    link firebase to display posts

--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  styleUrls: ['./topics.component.css'],
+  providers: [PostService]
 })
 export class TopicsComponent implements OnInit {
-
-  constructor() { }
+  posts: FirebaseListObservable<any[]>;
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts();
   }
+
+
 
 }
